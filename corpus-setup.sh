e2a6b7df5589bb0ded779dce644ac71d20d53566
ayseirmak <152956281+ayseirmak@users.noreply.github.com>
2025-06-15 22:17:33 +0100

    Create corpus-setup.sh

---
+++ corpus-setup.sh
@@ -0,0 +1,21 @@
+./AFLplusplus-latest-build.sh
+git clone --depth 1 https://github.com/llvm/llvm-test-suite.git
+cd llvm-test-suite
+cp -r SingleSource ../
+cd ..
+rm -rf llvm-test-suite
+mkdir -p llvmSS-c-corpus
+mkdir -p llvmSS-include
+
+c_count=0
+find SingleSource -type f \( -name "*.c" -o -name "*.h" \) \
+  ! -path "SingleSource/Regression/C/gcc-c-torture/*" | while read -r file; do
+
+    if [[ "$file" == *.c ]]; then
+        cp "$file" "llvmSS-c-corpus/test_${c_count}.c"
+        ((c_count++))
+    elif [[ "$file" == *.h ]]; then
+        cp "$file" llvmSS-include/
+    fi
+  done
+tar -czvf llvmSS-corpus.tar.gz -C /users/user42/ llvmSS-c-corpus llvmSS-include
