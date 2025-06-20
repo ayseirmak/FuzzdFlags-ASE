c6e09891bcc77d04a68d841ab331cd40beb7d887
ayseirmak <152956281+ayseirmak@users.noreply.github.com>
2025-06-20 17:22:48 +0100

    Update exp1-cov-afl-vanilla-setup.sh

--- coverage/exp1-cov-afl-vanilla-setup.sh
+++ coverage/exp1-cov-afl-vanilla-setup.sh
@@ -10,8 +10,21 @@ sudo chmod 777 /users/user42
 sudo apt-get update && sudo apt-get upgrade -y
 sudo chown -R user42:user42 /users/user42/
 
+cd /users/user42
 su - user42
-# I need to add coverage scripts here (wget)
+# wget default setup scripts
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/0-install-compilers-local.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/1-clone-llvm.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/2-llvm-cov-install.sh
+
+# wget baseline coverage experiment scripts
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/31-gfauto-multirep.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/41-cov-analysis-multirep.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/5-cov-table.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/51-inner-LH_file.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/61-backend-cov-analysis.sh
+wget https://raw.githubusercontent.com/ayseirmak/FuzzdFlags-ASE/refs/heads/main/coverage/62-middleend-cov-analysis.sh
+
 sudo chown -R user42:user42 /users/user42/
 chmod 777 *.sh
 
@@ -30,16 +43,16 @@ rm Pipfile.lock (if Python3.8 or above)
 ./dev_shell.sh.template
 cd ../..
 
-wget https://github.com/ayseirmak/FuzzdFlags/releases/download/v1.0-alpha/llvmSS-reindex-after-Cmin.tar.gz
-tar -zxvf llvmSS-reindex-after-Cmin.tar.gz
+wget https://github.com/ayseirmak/FuzzdFlags-ASE/releases/download/v1.0.0-alpha.1/llvmSS-minimised-corpus.tar.gz
+tar -zxvf llvmSS-minimised-corpus.tar.gz
 
 mkdir -p /users/user42/afl-vanilla-fuzz-o2
 mkdir -p /users/user42/afl-vanilla-fuzz-o3
 
-wget https://github.com/ayseirmak/FuzzdFlags/releases/download/v5.0/exp11-fuzz-results.tar.gz
-tar -zxvf exp11-fuzz-results.tar.gz -C /users/user42/afl-vanilla-fuzz-o2
-wget https://github.com/ayseirmak/FuzzdFlags/releases/download/v5.0/exp12-fuzz-results.tar.gz
-tar -zxvf exp12-fuzz-results.tar.gz -C /users/user42/afl-vanilla-fuzz-o3
+wget https://github.com/ayseirmak/FuzzdFlags-ASE/releases/download/v1.0.0-alpha.1/exp11-aflvan-O2-fuzz-results.tar.gz
+tar -zxvf exp11-aflvan-O2-fuzz-results.tar.gz -C /users/user42/afl-vanilla-fuzz-o2
+wget https://github.com/ayseirmak/FuzzdFlags-ASE/releases/download/v1.0.0-alpha.1/exp12-aflvan-O3-fuzz-results.tar.gz
+tar -zxvf exp12-aflvan-O3-fuzz-results.tar.gz -C /users/user42/afl-vanilla-fuzz-o3
 
 mkdir -p coverage-measurement
 cd coverage-measurement
@@ -47,10 +60,10 @@ mkdir -p afl-vanilla-cov/afl-vanilla-o2-cov afl-vanilla-cov/afl-vanilla-o3-cov
 cd ~
 
 cd /users/user42/coverage-measurement/afl-vanilla-cov/afl-vanilla-o2-cov 
-nohup /users/user42/3-gfauto-multidir.sh  /users/user42/afl-vanilla-fuzz-o2 /users/user42/coverage/llvm-clang-2  -O2 > cov-llvm2-input.log 2>&1 &
+nohup /users/user42/31-gfauto-multirep.sh  /users/user42/afl-vanilla-fuzz-o2 /users/user42/coverage/llvm-clang-2  -O2 > cov-llvm2-input.log 2>&1 &
 
 cd /users/user42/coverage-measurement/afl-vanilla-cov/afl-vanilla-o3-cov 
-nohup /users/user42/3-gfauto-multidir.sh  /users/user42/afl-vanilla-fuzz-o3 /users/user42/coverage/llvm-clang-3  -O3 > cov-llvm3-input.log 2>&1 &
+nohup /users/user42/31-gfauto-multirep.sh  /users/user42/afl-vanilla-fuzz-o3 /users/user42/coverage/llvm-clang-3  -O3 > cov-llvm3-input.log 2>&1 &
 
 cd ~
 nohup /users/user42/4-cov-analysis-multidir.sh ~/coverage-measurement/afl-vanilla-cov/afl-vanilla-o2-cov  /users/user42/coverage/llvm-clang-2 table_line_cov_afl_o2.csv > cov-mes-afl-o2.log 2>&1 &
