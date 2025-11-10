dbd42430a8df4620fa64dd6011d68b0345cc6123
a_irmak <a_irmak@node0.fdf-mut-blackbox.kclsystemfuzz-pg0.utah.cloudlab.us>
2025-11-10 15:38:59 -0700

    work on standalone mutators

---
+++ mutators/utils-fuzzer/GrayCCustomRandom.cpp
@@ -0,0 +1,11 @@
+#include "GrayCCustomRandom.h"
+
+GrayCCustomRandom *GrayCCustomRandom::m_instance = NULL;
+
+void GrayCCustomRandom::DeleteInstance(const unsigned long _seed) {
+  assert(m_instance);
+  // Sanity: the seed used to create and delete should match.
+  assert(_seed == m_instance->m_seed);
+  delete m_instance;
+  m_instance = NULL;
+}
